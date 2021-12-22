e8bb054fb75f1084b613213f578bb6ceee22533d
mudit tyagi <mudittyagi2002@gmail.com>
2021-12-22 12:54:51 +0530

    new programs added

--- for_loop.c
+++ for_loop.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-    int j,i, num;                                 // int i=0, num;
+    int j, i, num; // int i=0, num;
     printf("enter num= ");
     scanf("%d", &num);
-    for ( i=0, j=0 ; j< 5, i <= num; i++, j++)                //  for (; i <= num; i++)  
-    {                                                        //first block is optional
-        printf("%d%d\n",i,j);                     // 2nd block mein jo last expression hai wo consider hoga
-        i=i;                                            // third block is optional too
+    for (i = 1, j = 1; j < 5, i < num; i++, j++) //  for (; i <= num; i++)
+    {                                            //first block is optional
+        printf("%d   %d\n", i, j);               // 2nd block mein jo last expression hai wo consider hoga
+        i = i;                                   // third block is optional too
     }
-    
+
     return 0;
 }
