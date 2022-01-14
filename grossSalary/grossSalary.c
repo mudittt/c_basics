a1644249d213dd797179db3cdccf0dbccb1840b3
mudit tyagi <mudittyagi2002@gmail.com>
2022-01-14 12:10:44 +0530

    added new college work

---
+++ grossSalary/grossSalary.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float basix, DA, HRA, gross;
+    printf("Enter basic salary : ");
+    scanf("%f", &basix);
+
+    if (basix <= 10000)
+    {
+        HRA = basix * 0.2;
+        DA = basix * 0.8;
+    }
+    else if (basix <= 20000)
+    {
+        HRA = basix * 0.25;
+        DA = basix * 0.9;
+    }
+    else
+    {
+        HRA = basix * 0.3;
+        DA = basix * 0.95;
+    }
+    gross = basix + DA + HRA;
+    printf("The gross salary is : %.2f \n", gross);
+
+    return 0;
+}
