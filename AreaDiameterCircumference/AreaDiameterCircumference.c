46b6ed6442f89bce0e55d00f7005ba45b429c45e
mudit tyagi <mudittyagi2002@gmail.com>
2022-01-28 11:33:56 +0530

    pointer functions to find out Area-Diameter-Circumference

---
+++ AreaDiameterCircumference/AreaDiameterCircumference.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+
+float Diameter(int p, int q, int r, int s)
+{
+    int temp1, temp2;
+    double temp3;
+    temp1 = pow((r - p), 2);
+    temp2 = pow((s - q), 2);
+    temp3 = sqrt(temp1 + temp2);
+    printf("\nThe diameter of this circle is : %f\n", temp3);
+}
+float min_dist(int p, int q, int r, int s)
+{
+    int temp1, temp2;
+    double temp3;
+    temp1 = pow((r - p), 2);
+    temp2 = pow((s - q), 2);
+    temp3 = sqrt(temp1 + temp2);
+
+    return temp3;
+}
+float areaCircle(float (*hehe)(int, int, int, int), int a, int b, int c, int d)
+{
+    printf("\nThe area of the circle is : %f\n", 3.14 * hehe(a, b, c, d) * hehe(a, b, c, d));
+}
+
+float CirCumFerence(float (*hehe)(int, int, int, int), int a, int b, int c, int d)
+{
+    printf("\nThe circumference of the circle is : %f\n", 3.14 * hehe(a, b, c, d) * 2);
+}
+
+int main()
+{
+    int x1, y1, x2, y2;
+    printf("Enter the x-coordinate of first point : ");
+    scanf("%d", &x1);
+    printf("Enter the y-coordinate of first point : ");
+    scanf("%d", &y1);
+    printf("Enter the x-coordinate of second point : ");
+    scanf("%d", &x2);
+    printf("Enter the y-coordinate of second point : ");
+    scanf("%d", &y2);
+
+    float (*mud)(int, int, int, int);
+    mud = &min_dist;
+    Diameter(x1, y1, x2, y2);
+    areaCircle(mud, x1, y1, x2, y2);
+    CirCumFerence(mud, x1, y1, x2, y2);
+
+    return 0;
+}
