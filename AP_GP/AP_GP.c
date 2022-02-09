324b26db190cc3b8bf00cc216ec2e21400da2e1e
mudit tyagi <mudittyagi2002@gmail.com>
2022-02-09 17:36:11 +0530

    removed some blank spaces

--- AP_GP/AP_GP.c
+++ AP_GP/AP_GP.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(int argc, char const *argv[])
-{
+int main () {
     int a1, d, a2, r;
     int n1, n2;
     /* FOR AP */
@@ -10,12 +9,9 @@ int main(int argc, char const *argv[])
     scanf("%d", &a1);
     printf("Enter the common difference of your AP :");
     scanf("%d", &d);
-
     printf("Enter the number of terms you want of your AP : ");
     scanf("%d", &n1);
-
-    for (int i = 1; i <= n1; i++)
-    {
+    for (int i = 1; i <= n1; i++) {
         printf(" %d\n", a1 + ((i - 1) * d));
     }
     printf("The sum of the above number of elements is : %d\n", (n1 * ((2 * a1) + ((n1 - 1) * d))) / 2);
@@ -27,13 +23,9 @@ int main(int argc, char const *argv[])
     scanf("%d", &r);
     printf("Enter the number of terms you want of your GP : ");
     scanf("%d", &n2);
-
-    for (int i = 1; i <= n2; i++)
-    {
+    for (int i = 1; i <= n2; i++) {
         printf(" %lf\n", (a2 * pow(r, (i - 1))));
     }
-
     printf("The sum of the above number of elements is : %lf", (a2 * (pow(r, n2) - 1)) / (r - 1));
-
     return 0;
 }
