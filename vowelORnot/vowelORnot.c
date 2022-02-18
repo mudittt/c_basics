5f89f5d1307b7d23a3eaaeb7ac498bbc6c4ee249
mudit tyagi <mudittyagi2002@gmail.com>
2022-02-18 12:23:21 +0530

    to check whether an alphabet is vowel or not

--- vowelORnot/vowelORnot.c
+++ vowelORnot/vowelORnot.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-int main(int argc, char const *argv[])
+int main ()
 {
     char alphabet;
     printf("Enter an alphabet : ");
     scanf("%c", &alphabet);
-    if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u')
-    {
+    if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u'
+    || alphabet == 'A' || alphabet == 'E' || alphabet == 'I' || alphabet == 'O' || alphabet == 'U') {
         printf("Entered alphabet is a VOWEL.\n");
     }
-    else
-    {
+    else {
         printf("Entered alphabet is not a VOWEL.\n");
     }
 
