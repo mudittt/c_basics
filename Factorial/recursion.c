9ce0b043c2b775e429799b91860907f2a258e33c
mudit tyagi <mudittyagi2002@gmail.com>
2022-02-22 15:14:27 +0530

    recursion file and it's output

---
+++ Factorial/recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int factorial(int num){
+    if (num==1 || num==0){
+       return 1;
+    }
+    else{
+        return num*factorial(num-1);
+    }
+}
+int main (){
+    int a;
+    printf("enter the number you want factorial of = ");
+    scanf("%d", &a);
+    printf("%d", factorial(a));
+    return 0;
+}
+
